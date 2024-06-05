06515accdbe6aaf5b787d7746775b3aaabc52b53
me <sadmanislam@re-boyds-145-109-129-194.wireless.hva.nl>
2024-06-05 14:40:57 +0200

    Methodes voor traject class

--- classes/traject.py
+++ classes/traject.py
@@ -1,5 +1,25 @@
 class Traject:
     
-    def __init__(self) -> None:
-        self.stations
-        self.duration
+    def __init__(self, stations) -> None:
+        self.stations = stations
+        self.duration: int = 0
+
+
+    def make_traject(self) -> None:
+    """ Makes a traject """
+        pass
+
+
+    def calc_duration(self) -> bool:
+    """ Calculates the time that a traject takes. Returns true if the traject is below the max time, else false. """
+
+        #TODO
+        traject_duraction = 0
+        
+        self.duration = traject_duraction
+        pass
+
+
+    def comp_traject(self) -> bool:
+    """ Compares valiable trajects to determine best option."""
+        pass
