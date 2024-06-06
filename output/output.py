4d8bb2bfa5c6f2efd22a44305ec806f27a533ecf
ste-kooi <steven81@hotmail.nl>
2024-06-06 13:15:54 +0200

    wrote output() and calculate_score()

--- output/output.py
+++ output/output.py
@@ -1,13 +1,47 @@
-def output():
+from classes import Model
+import csv
+
+def output(model: Model):
     """
-    This function generetes an output file in csv format.
+    This function generates an output file in csv format.
+    It takes values from a model
     
     """
 
-    # Header
-    
-    # Body
+    # Initiate data list of lists with a header
+    data = [['train', 'stations']]
 
-    # Footer
+    
+    # get all routes from the model
+    for route in model.routes:
+        data.append([route.train_id, f"{route.stations}"])
+
+    # footer of the table contains the score of the model
+    data.append(['score', calculate_score(model)])
+
+    # write data to CSV file
+    filename = "output/model_output.csv"
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+
+    print(f"Data has been written to {filename}")
         
-    pass
+
+
+def calculate_score(model) -> float:
+    """
+    This function calculates a K score from a model following the formula:
+
+    K = p * 1000 - (T * 100 + Min)
+
+    K:   quality of route planning
+    p:   fraction of covered connections
+    T:   number of routes
+    Min: total minutes of routes
+
+    """
+    T = len(model.routes)
+    p = model.coverage()
+    Min = model.total_time()
+    return p * 1000 - (T * 100 + Min)
