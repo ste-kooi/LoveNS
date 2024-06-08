a1b3495128d6c702eb0190c96e1a971125b4936f
ste-kooi <steven81@hotmail.nl>
2024-06-08 13:58:10 +0200

    updated make_routes to work with the right class methods

--- classes/model.py
+++ classes/model.py
@@ -162,17 +162,23 @@ class Model:
 
         -- dit gaat uiteindelijk naar algorithms --
         """
-        for train_id in range(7):
+        for train_id in range(1,8):
+
+            # pick a random station
             random_station_name = random.choice(list(self.stations.keys()))
             current_station = self.stations[random_station_name]
             self.add_route(current_station, train_id)
             
             while self.routes[train_id].duration < 120:
-                random_connection = random.choice(list(current_station.connections.keys()))
+                if len(current_station.connections) > 1:
+                    random_connection = random.choice(list(current_station.connections.keys()))
+                else:
+                    random_connection = next(iter(current_station.connections))
+
                 new_connection = current_station.connections[random_connection]
+                self.routes[train_id].add_station(new_connection.station1)
                 current_station = new_connection.station2
                     
-                self.routes[train_id].add_interconnection(new_connection)
                 self.routes[train_id].add_station(current_station)
 
 if __name__ == '__main__':
