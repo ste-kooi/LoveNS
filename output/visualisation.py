4f1f75b79925c41c76b5c951ee22910863fcd63c
ste-kooi <steven81@hotmail.nl>
2024-06-10 18:11:25 +0200

    added line offset to see multiple routes between stations

--- output/visualisation.py
+++ output/visualisation.py
@@ -1,7 +1,9 @@
 import sys
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 from itertools import cycle
+from output import output
 
 # Add the path to the classes folder to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../classes')))
@@ -22,7 +24,7 @@ def visualise(model: Model):
         x, y = station.location
         ax.plot(x, y, 'o')
         ax.annotate(station.name, (x, y), textcoords="offset points", xytext=(5,5), ha='center')
-              
+            
     # plot routes
 
     # create color cycle
@@ -30,9 +32,15 @@ def visualise(model: Model):
 
     for route in model.routes.values():
         # Extract x and y coordinates for each station in the route
-        route_x = [station.location[0] for station in route.stations]
-        route_y = [station.location[1] for station in route.stations]
+        route_x = np.array([station.location[0] for station in route.stations])
+        route_y = np.array([station.location[1] for station in route.stations])
         
+        # Apply a small random offset to separate overlapping lines
+        offset_x = np.random.uniform(-0.01, 0.01, size=route_x.shape)
+        offset_y = np.random.uniform(-0.01, 0.01, size=route_y.shape)
+        route_x += offset_x
+        route_y += offset_y
+
         # Plot line connecting the stations
         color = next(colors)
         ax.plot(route_x, route_y, color = color)
@@ -46,5 +54,6 @@ def visualise(model: Model):
 
 if __name__ == '__main__':
     mod = Model('Holland')
-    mod.make_routes()
+    mod.random_routes(7)
+    output(mod)
     visualise(mod)
