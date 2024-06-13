6f110012dc77a752ad699ddf6bb43f2ec25f2796
ste-kooi <steven81@hotmail.nl>
2024-06-13 17:22:28 +0200

    now uses get methods

--- algorithms/randomise.py
+++ algorithms/randomise.py
@@ -1,41 +1,33 @@
-import sys
-import os
+from classes.model import Model
 import random
+import copy
 
-# Add the path to the classes folder to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../classes')))
-from model import Model
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../output')))
-from output import output
-from visualisation import visualise
-
-
-
-def random_routes(model, amount):
+def random_routes(model: Model, amount):
     """
     Generates random routes for the model, ensuring each route does not exceed 120 minutes.
 
     Parameters
     ---------
+    model: Model
+        The model to run.
     amount : int
         The number of routes to create.
 
-    -- dit gaat uiteindelijk naar algorithms --
     """
-    for train_id in range(1, amount + 1):
+    max_time = 120
+    for route_id in range(1, amount + 1):
 
-        # pick random starting station
-        current_station = model.stations[random.choice(list(model.stations))]
+        # pick a random starting station
+        current_station = random.choice(list(model.stations.values()))
 
         # add new route from that station
-        model.add_route(current_station, train_id)
+        model.add_route(current_station, route_id)
 
         # pick a station from the connections list
-        while model.routes[train_id].duration < 120:
+        while model.routes[route_id].duration < max_time:
             # get stations from connections and filter out visited stations
-            possible_connections = [station for station in current_station.connections 
-                                    if model.stations[station] not in model.routes[train_id].stations]
+            possible_connections = [station for station in current_station.get_connections() 
+                                    if model.get_station(station) not in model.get_route(route_id).get_stations()]
             if not possible_connections:
                 break    
             
@@ -44,14 +36,12 @@ def random_routes(model, amount):
             next_connection = current_station.connections[next_station]
 
             # if addition of station to route takes to long, break
-            if model.routes[train_id].duration + next_connection.time > 120:
+            if model.get_route(route_id).duration + next_connection.time > max_time:
                 break
 
             # add station to route
             current_station = model.stations[next_station]
-            model.routes[train_id].add_station(current_station)
-if __name__ == '__main__':
-    model = Model('Holland')
-    random_routes(model, 7)
-    output(model)
-    visualise(model)
+            model.routes[route_id].add_station(current_station)
+  
+
+
