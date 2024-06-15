e4773863991ce5a94b9da6bcabdf046e5b346a6c
raniserre <raniserre@gmail.com>
2024-06-15 20:59:52 +0200

    depth_first: make_children, run, get_next_option

--- algorithms/depth_first.py
+++ algorithms/depth_first.py
@@ -6,7 +6,8 @@ class Depth_first_route:
         self.model = copy.deepcopy(model)
         self.begin = begin_station
         
-        self.states = []
+        self.options = []
+        self.best_trains: dict[int, list[Route]] #int is train_id
         
         if mapname == "Holland":
             self.amount_trains = 7
@@ -16,17 +17,24 @@ class Depth_first_route:
             self.amount_trains = 20
             self.timeframe = 180
     
-    def make_children(self):
+    def make_children(self, station):
         current_station = self.begin
-        possible_connections = list(self.model.connections.keys())
+        possible_connections: list[str] = list(current_station.connections.keys())
         for station in possible_connecitons:
-            self.states.append(station)
+            self.options.append(station)
     
     def get_next_step(self):
-        return 
+        return self.states.pop()
     
     def check_score(self):
         self.model.calculate_score()
     
     def run(self):
-        pass        
+        for train_id in range(1, amount_trains + 1):
+            self.best_trains[train_id] = []
+            new_route = self.model.add_route(self.begin, train_id)
+            self.make_children(self.begin)
+            
+            while self.options and new_route.duration < 120:
+                
+                # route moet herbruikt worden en dan met een station toevoegen om alle mogelijkheden te onderzoeken
