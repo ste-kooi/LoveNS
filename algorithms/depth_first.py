8c120ce76b986fea3fa26485ac9b9de9bc67b204
raniserre <raniserre@gmail.com>
2024-06-15 23:07:53 +0200

    depth first check_score. improvements to other functions in depth_first

--- algorithms/depth_first.py
+++ algorithms/depth_first.py
@@ -1,40 +1,53 @@
 import copy
 from classes.model import Model
+from classes.station import Station
+from classes.route import Route
+from classes.connection import Connection
+
 
 class Depth_first_route:
-    def __init__(self, model: Model, mapname: str, begin_station: Station):
+    def __init__(self, model: Model, begin_station: Station, train_id: int, max_time: int):
         self.model = copy.deepcopy(model)
-        self.begin = begin_station
-        
-        self.options = []
-        self.best_trains: dict[int, list[Route]] #int is train_id
-        
-        if mapname == "Holland":
-            self.amount_trains = 7
-            self.timeframe = 120
-            
-        elif mapname == "Nederland":
-            self.amount_trains = 20
-            self.timeframe = 180
+        self.best_route = Route(train_id)
+        self.best_route.add_station(begin_station)
+        self.options: list[Route] = [self.best_route]
+        self.best_solution = 0
+        self.max_time = max_time
+
+    def make_children(self, this_route):
+        if this_route:
+            current_station = this_route.get_stations()[-1]
+            possible_connections: set[str] = set(current_station.connections.keys())
+            for station in possible_connections:
+                if station == current_station.name:
+                    continue
+                new_station = self.model.stations[station]
+                route_option = copy.deepcopy(this_route)
+                route_option = route_option.add_station(new_station)
+                self.options.append(route_option)
     
-    def make_children(self, station):
-        current_station = self.begin
-        possible_connections: list[str] = list(current_station.connections.keys())
-        for station in possible_connecitons:
-            self.options.append(station)
-    
-    def get_next_step(self):
-        return self.states.pop()
+    def get_next_option(self):
+        return self.options.pop()
     
     def check_score(self):
-        self.model.calculate_score()
+        score = self.model.calculate_score()
+        if score > self.best_solution:
+            self.best_solution = score
+            print(f"new best score: {self.best_solution}")
+            return True
+        return False
     
-    def run(self):
-        for train_id in range(1, amount_trains + 1):
-            self.best_trains[train_id] = []
-            new_route = self.model.add_route(self.begin, train_id)
-            self.make_children(self.begin)
+    def run(self):            
+        while self.options:    
+            this_route = self.get_next_option()
             
-            while self.options and new_route.duration < 120:
+            if this_route:
+                if this_route.duration < self.max_time:
+                    self.make_children(this_route)
                 
-                # route moet herbruikt worden en dan met een station toevoegen om alle mogelijkheden te onderzoeken
+                    if self.check_score():
+                        self.best_route = this_route
+                        self.model.remove_route(this_route.train_id)
+                        self.model.add_excisting_route(this_route)
+        
+        return self.best_route
