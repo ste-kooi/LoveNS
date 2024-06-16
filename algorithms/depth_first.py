96194a10ddda1016b2dcc41b09049425d8220186
raniserre <raniserre@gmail.com>
2024-06-16 15:58:21 +0200

    bug fixes

--- algorithms/depth_first.py
+++ algorithms/depth_first.py
@@ -7,7 +7,7 @@ from classes.connection import Connection
 
 class Depth_first_route:
     def __init__(self, model: Model, begin_station: Station, train_id: int, max_time: int):
-        self.model = copy.deepcopy(model)
+        self.model = model
         self.best_route = Route(train_id)
         self.best_route.add_station(begin_station)
         self.options: list[Route] = [self.best_route]
@@ -15,16 +15,17 @@ class Depth_first_route:
         self.max_time = max_time
 
     def make_children(self, this_route):
-        if this_route:
-            current_station = this_route.get_stations()[-1]
-            possible_connections: set[str] = set(current_station.connections.keys())
-            for station in possible_connections:
-                if station == current_station.name:
-                    continue
-                new_station = self.model.stations[station]
-                route_option = copy.deepcopy(this_route)
-                route_option = route_option.add_station(new_station)
-                self.options.append(route_option)
+        current_station = this_route.get_stations()[-1]
+        possible_connections: dict[str, Connection] = current_station.get_connections()
+        for station in possible_connections.keys():
+            if station == current_station.name:
+                continue
+            if not this_route.check_connection(possible_connections[station]):
+                continue
+            new_station = self.model.stations[station]
+            route_option = copy.deepcopy(this_route)
+            route_option.add_station(new_station)
+            self.options.append(route_option)
     
     def get_next_option(self):
         return self.options.pop()
@@ -41,13 +42,14 @@ class Depth_first_route:
         while self.options:    
             this_route = self.get_next_option()
             
-            if this_route:
-                if this_route.duration < self.max_time:
-                    self.make_children(this_route)
+            if this_route.duration < self.max_time:
+                self.make_children(this_route)
+                self.model.add_excisting_route(this_route)
                 
-                    if self.check_score():
-                        self.best_route = this_route
-                        self.model.remove_route(this_route.train_id)
-                        self.model.add_excisting_route(this_route)
+                if self.check_score():
+                    self.best_route = this_route
+                    
+            self.model.remove_route(this_route.train_id)
+            self.model.add_excisting_route(self.best_route)
         
-        return self.best_route
+        return self.model
