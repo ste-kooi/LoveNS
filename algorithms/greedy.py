f8b3a66824961e0769ac6a1948f4c44e52dc0408
lyarnoda <sadieboy12@gmail.com>
2024-06-17 15:14:03 +0200

    greedy need help

--- algorithms/greedy.py
+++ algorithms/greedy.py
@@ -12,10 +12,9 @@ class Greedy():
 
         self.mod = Model("Holland")
         self.route = Route(self.route_nr)
+        self.temp_route = copy.copy(Route(self.route_nr))
 
     def create_greedy(self):
-
-
 
         for x in range(6):
             self.route_nr += 1
@@ -26,13 +25,36 @@ class Greedy():
                 sorted_connections = sorted(random_station.connections.values(), key=lambda con: con.time)
 
                 for connection in sorted_connections:
+
                     if connection.station1 == random_station:
                         destination_station = connection.station2
                     else:
                         destination_station = connection.station1
 
                     self.route.add_station(destination_station)
+                    best_score = self.mod.calculate_score() #hoe vergelijk ik scores?
 
-                    best_score = self.mod.calculate_score()
+                    for temp_connection in sorted_connections[1:]: # begint een positie verder zodat je steeds 2 connecties met elkaar vergelijkt.
+                        if connection.station1 == random_station:
+                            temp_destination_station = connection.station2
+                        else:
+                            temp_destination_station = connection.station1
+
+                        self.temp_route.add_station(temp_destination_station)
+                        temp_best_score = self.mod.calculate_score() #hoe vergelijk ik deze score met best_score
+
+                        if temp_best_score > best_score:
+                            best_score = temp_best_score
+                            self.route.remove_last_station()
+                            self.route.add_station(temp_destination_station)
+                            random_station = temp_destination_station
+                            break
+                        else:
+                            random_station = destination_station
+                            break
+
+
+            print(self.route)
+            print(self.temp_route)
 
 
