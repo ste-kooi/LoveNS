76f123a89f7e4bf9339afc5c343fe33d86d97f73
ste-kooi <steven81@hotmail.nl>
2024-06-19 20:34:25 +0200

    added mutate_end_of_routes and delete_routes, run() can randomly pick a method

--- algorithms/Hillclimber.py
+++ algorithms/Hillclimber.py
@@ -1,5 +1,5 @@
 from classes.model import Model
-from algorithms.randomise import random_reconfigure_route
+from algorithms.randomise import *
 from output.output import output
 from output.visualisation import visualise
 
@@ -12,7 +12,7 @@ import random
 class HillClimber:
     """
     The HillClimber class that changes a random route in the model to random valid route. Each imporvement
-    or equivalent solution is kept for the next iteration. 
+    or equivalent solution is kept for the next iteration.
     """
     def __init__(self, model: Model) -> None:
         self.model = copy.deepcopy(model)
@@ -26,12 +26,40 @@ class HillClimber:
         random_reconfigure_route(new_model, random_route_id)
 
 
+
     def mutate_model(self, new_model, number_of_routes=1):
         """
         Changes the value of a number of routes with a random valid value.
         """
         for _ in range(number_of_routes):
             self.mutate_single_route(new_model)
+
+
+    def mutate_end_of_routes(self, new_model: Model):
+        random_route_id = random.choice(list(new_model.routes))
+        for _ in range(random.randint(1,3)):
+            new_model.get_route(random_route_id).remove_last_station()
+
+        random_route_id_2 = random.choice(list(new_model.routes))
+        for _ in range(random.randint(1,3)):
+            new_model.get_route(random_route_id_2).remove_last_station()
+
+        new_model.update_used_connections()
+        if new_model.get_route(random_route_id).get_stations():
+            random_extend_route(new_model, random_route_id)
+        else:
+            start_station = new_model.stations[random.choice(new_model.get_stations_unused_connections())]
+            random_single_route(new_model, start_station, random_route_id)
+
+    def delete_routes(self, new_model: Model):
+
+        route_1 = random.choice(list(new_model.routes))
+        route_2 = random.choice(list(new_model.routes))
+        new_model.remove_route(route_1)
+        new_model.remove_route((route_2))
+        start_station = new_model.stations[random.choice(new_model.get_stations_unused_connections())]
+        random_single_route(new_model, start_station, route_1)
+
 
 
     def check_score(self, new_model: Model):
@@ -53,12 +81,15 @@ class HillClimber:
         """
         for iteration in range(iterations):
 
-            print(f'Iteration {iteration}/{iterations}, current value: {self.score}') 
+            print(f'Iteration {iteration}/{iterations}, current value: {self.score}')
 
             # create copy of the model to simulate the change
             new_model = copy.deepcopy(self.model)
 
-            self.mutate_model(new_model, number_of_routes=mutate_routes_number)
+            # pick which change to make from parameters
+            method = random.choice([self.mutate_single_route, self.mutate_end_of_routes, self.delete_routes])
+            method(new_model)
 
-        
             self.check_score(new_model)
+
+
