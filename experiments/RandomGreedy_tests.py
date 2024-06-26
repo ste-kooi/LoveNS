b658acc8e0bdde16436ebd7920b643db7ba9940b
lyarnoda <sadieboy12@gmail.com>
2024-06-26 14:15:59 +0200

    bug fix

--- experiments/RandomGreedy_tests.py
+++ experiments/RandomGreedy_tests.py
@@ -5,34 +5,34 @@ from output.visualisation import visualise
 
 import time
 
-def experiment()
-    best_outputs = 5
-    best_models = []
-    iteration_total = 0
-    start = time.time()
 
-    for best in range(best_outputs):
-        model = Model("Nederland")
-        gred = RandomGreedy(model)
+best_outputs = 5
+best_models = []
+iteration_total = 0
+start = time.time()
 
-        start_gred = time.time()
-        while time.time() - start_gred < 600:
-            best_model = gred.run(1)
-            iteration_total += 1
+for best in range(best_outputs):
+    model = Model("Nederland")
+    gred = RandomGreedy(model)
 
-        best_models.append(best_model)
+    start_gred = time.time()
+    while time.time() - start_gred < 600:
+        best_model = gred.run(1)
+        iteration_total += 1
 
-    output_multiple(best_models, f"experiments/NL_RandomGreedy/RandomGreedy_2")
-    visualise(best_model, f"experiments/NL_RandomGreedy/RandomGreedy_img2")
+    best_models.append(best_model)
 
-    end = time.time()
+output_multiple(best_models, f"experiments/NL_RandomGreedy/RandomGreedy_2")
+visualise(best_model, f"experiments/NL_RandomGreedy/RandomGreedy_img2")
 
-    print(f'Total time: {best_model.total_time()}')
-    print(f'Coverage: {best_model.get_coverage()}')
-    print(f'Final score: {gred.score}')
-    print(f'Runtime: {end - start}')
-    print(f'States: {gred.states}')
-    print(f'Iterations used: {iteration_total}')
+end = time.time()
+
+print(f'Total time: {best_model.total_time()}')
+print(f'Coverage: {best_model.get_coverage()}')
+print(f'Final score: {gred.score}')
+print(f'Runtime: {end - start}')
+print(f'States: {gred.states}')
+print(f'Iterations used: {iteration_total}')
 
 
 
